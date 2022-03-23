7047a6300efac418e9191dc2602cfb175c8ed29c
Akash <akash.nits@outlook.com>
2022-03-23 13:09:07 +0530

    Create Nth Fibonacci.java

---
+++ Nth Fibonacci.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Program {
+  public static int getNthFib(int n) {
+    // Write your code here.
+		// base condition
+		if(n == 1){
+			return 0;
+		}
+		
+		if( n == 2){
+			return 1;
+		}
+		
+		return getNthFib(n-1) + getNthFib(n-2);
+  }
+}
