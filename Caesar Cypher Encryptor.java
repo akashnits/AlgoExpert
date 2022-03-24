4697cfb6635780aa18471df3326941fe9810deb6
Akash <akash.nits@outlook.com>
2022-03-24 12:16:57 +0530

    Create Caesar Cypher Encryptor.java

---
+++ Caesar Cypher Encryptor.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Program {
+  public static String caesarCypherEncryptor(String str, int key) {
+    // Write your code here.
+		StringBuilder sb = new StringBuilder();
+		int normalizedKey = key % 26;
+		for(int i=0; i < str.length(); i++){
+			int ascii;
+			char c = str.charAt(i);
+			ascii = (int) c + normalizedKey;
+			if(ascii <= 122){
+				c = (char) ascii;
+			}else{
+				c = (char) (96+ (ascii - 122));
+			}
+			sb.append(c);
+		}
+    return sb.toString();
+  }
+}
