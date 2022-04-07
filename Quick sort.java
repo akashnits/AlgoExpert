24d633ee9f9bf702eb736a35291b9560a055fb2b
Akash <akash.nits@outlook.com>
2022-04-08 01:11:19 +0530

    Create Quick sort.java

---
+++ Quick sort.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Program {
+  public static int[] quickSort(int[] array) {
+		quickSortHelper(array, 0, array.length-1);
+    return array;
+  }
+	
+	private static void quickSortHelper(int[] array, int start, int end){
+		if(start >= end){
+			return;
+		}
+		
+		int pivot = start;
+		int left = start+1;
+		int right = end;
+		
+		while(left <= right){
+			if(array[left] > array[pivot] && array[right] < array[pivot]){
+				swap(array, left, right);
+			}
+			
+			if(array[left] <= array[pivot]){
+				left++;
+			}
+			
+			if(array[right] >= array[pivot]){
+				right--;
+			}
+		}
+		
+		// swap pivot with right
+		swap(array, right, pivot);
+		
+		quickSortHelper(array, start, right-1);
+		quickSortHelper(array, right+1, end);
+	}
+	
+	private static void swap(int[] array, int i, int j){
+		int temp = array[i];
+		array[i]= array[j];
+		array[j] = temp;
+	}
+}
